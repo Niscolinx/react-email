e5d3f74bfb816ae188a0fed173bd3d3419853518
Niscolinx <munisco12@gmail.com>
2023-09-05 18:04:00 +0100

    added assets

--- imageExports.ts
+++ imageExports.ts
@@ -5,3 +5,16 @@ export const accountDeactivated = `${baseUrl}/images/accountDeactivated.png`
 export const accountReactivated = `${baseUrl}/images/accountReactivated.png`
 export const adExpiration = `${baseUrl}/images/adExpiration.png`
 export const checkBoxFill = `${baseUrl}/images/checkBoxFill.png`
+export const createdInvoice = `${baseUrl}/images/createdInvoice.png`
+export const dueInvoice = `${baseUrl}/images/dueInvoice.png`
+export const facebook = `${baseUrl}/images/facebook.png`
+export const instagram = `${baseUrl}/images/instagram.png`
+export const twitter = `${baseUrl}/images/twitter.png`
+export const linkedin = `${baseUrl}/images/linkedin.png`
+export const invoiceReminder = `${baseUrl}/images/invoiceReminder.png`
+export const passwordReset = `${baseUrl}/images/passwordReset.png`
+export const pendingInvoice = `${baseUrl}/images/pendingInvoice.png`
+export const welcome = `${baseUrl}/images/welcome.png`
+export const withdrawalRejected = `${baseUrl}/images/withdrawalRejected.png`
+export const withdrawalSuccess = `${baseUrl}/images/withdrawalSuccess.png`
+
