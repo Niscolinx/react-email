d5806727aaa1c5a92d47b731e32164f0d0d95f1d
Niscolinx <munisco12@gmail.com>
2023-09-05 21:05:03 +0100

    working on emails

--- emails/index.tsx
+++ emails/index.tsx
@@ -5,37 +5,84 @@ import {
 	Html,
 	Img,
 	Preview,
+	render,
 	Section,
 	Tailwind,
 	Text,
 } from "@react-email/components";
+import { promises as fs } from "fs";
+import { dirname, join as pathJoin } from "path";
 import React from "react";
 import { logo } from "../imageExports";
 
 const previewText = "Welcome";
-export default function Email() {
+export default function Welcome() {
 	return (
 		<Html>
 			<Head />
 			<Preview>{previewText}</Preview>
-			<Tailwind
-				config={{
-					theme: {
-						extend: {
-							fontFamily: {
-								"sf-pro": ["SF Pro Display", "sans-serif"],
-							},
-						},
-					},
+
+			<Body
+				style={{
+					background: "#ffffff",
 				}}
 			>
-				<Body className="bg-[#E5EFFF] font-sf-pro">
-					<Container>
-					
-					<Img src={logo} alt='logo'/>
-					</Container>
-				</Body>
-			</Tailwind>
+				<Container
+					style={{
+						display: "grid",
+						justifyContent: "center",
+						justifyItems: "center",
+						textAlign: "center",
+					}}
+				>
+					<Section
+						style={{
+							display: "flex",
+							// justifyItems: "center",
+							// justifyContent: "center",
+							width: "100%",
+
+						}}
+					>
+						<Img src={logo} alt="logo" style={{
+							width: '100px',
+							display: 'flex',
+							justifySelf: 'center'
+						}} />
+					</Section>
+					<Text style={{
+						background: 'yellow'
+					}}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+						exercitationem molestiae nobis! Officia ipsa expedita illum incidunt
+						numquam, obcaecati reprehenderit quis, eligendi temporibus deleniti
+						porro libero harum tempore omnis nulla. Lorem ipsum dolor sit, amet
+						consectetur adipisicing elit. Perspiciatis, vitae nobis impedit
+						animi mollitia ullam voluptatibus accusantium repudiandae, dicta,
+						corrupti consequuntur saepe. Tempore temporibus esse ipsa, iure
+						adipisci aut. Facere!
+					</Text>
+				</Container>
+			</Body>
 		</Html>
 	);
 }
+
+const basePath = pathJoin(process.cwd(), "out");
+
+const html = render(<Welcome />, {
+	pretty: true,
+});
+
+const writeFile = async (path: string) => {
+	try {
+		await fs.mkdir(path);
+		fs.writeFile(pathJoin(basePath, "Welcome.html"), html, "utf-8");
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+writeFile(basePath);
+
+console.log(basePath);
