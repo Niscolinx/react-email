6cd9b4ca69dfaa536de62aab4b039c3db6fab538
Niscolinx <munisco12@gmail.com>
2023-09-05 17:59:57 +0100

    added assets

--- emails/index.tsx
+++ emails/index.tsx
@@ -1,12 +1,8 @@
 import { Button,Html, Img } from "@react-email/components";
-import Image from "next/image";
 import React from "react";
-// import Logo from '../public/images/dark-logo.png'
-import Logo from '../public/images/dark-logo.png'
 
-const baseUrl = process.env.BASE_URL
 
-const image = `${baseUrl}/static/plaid.png`
+
 
 
 
@@ -19,7 +15,7 @@ export default function Email() {
 				href="https://example.com"
 				style={{ background: "#000", color: "#fff" }}
 			>
-				<Img src={image} />
+				{/* <Img src={image} /> */}
 				Click me
 			</Button>
 		</Html>
---
+++ imageExports.ts
@@ -0,0 +1,7 @@
+const baseUrl = process.env.BASE_URL
+
+export const logo = `${baseUrl}/images/dark-logo.png`
+export const accountDeactivated = `${baseUrl}/images/accountDeactivated.png`
+export const accountReactivated = `${baseUrl}/images/accountReactivated.png`
+export const adExpiration = `${baseUrl}/images/adExpiration.png`
+export const checkBoxFill = `${baseUrl}/images/checkBoxFill.png`
