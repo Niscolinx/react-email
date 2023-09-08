81dfbea8983d0d1316c946e5eae3848d6a57f61f
Niscolinx <munisco12@gmail.com>
2023-09-08 15:34:10 +0100

    working on export

--- export-to-html.ts
+++ export-to-html.ts
@@ -3,25 +3,12 @@ import { promises as fs } from "fs";
 import { join as pathJoin } from "path";
 import { CONTENT_DIR, getEmails } from "@/utils/get-emails";
 
-export const dynamicParams = true;
-
-export async function generateStaticParams() {
-	const { emails } = await getEmails();
-
-	const paths = emails.map((email) => {
-		return { slug: email };
-	});
-
-	return paths;
-}
 
 async function exportToHtml() {
 	const { emails, filenames } = await getEmails();
 
 	console.log(emails)
-	console.log(filenames)
-	const params = generateStaticParams()
-	console.log({params})
+	
 
 
 	// const template = filenames.filter((email) => {
